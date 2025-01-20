a77c36b3fb26440b45d6093b81961d256865c1ba
Fabian Langguth <fabian.langguth@gmail.com>
2025-01-20 22:43:10 +0100

    add eval function

--- main.py
+++ main.py
@@ -29,10 +29,22 @@ class BigramLanguageModel(nn.Module):
         return idx
 
 
-def loss_fn(model, x, y, reduce=True):
+def loss_fn(model: nn.Module, x: mx.array, y: mx.array, reduce=True):
     logits = model(x)
     losses = nn.losses.cross_entropy(logits, y)
     return mx.mean(losses) if reduce else mx.mean(losses, axis=(-1, -2))
+
+
+def eval_fn(model: nn.Module, dataset: np.array, context_size: int, batch_size: int):
+    inputs, targets = map(mx.array, Data.to_samples(context_size, dataset))
+    loss = 0
+    for s in range(0, targets.shape[0], batch_size):
+        bx, by = inputs[s : s + batch_size], targets[s : s + batch_size]
+        bx, by = map(mx.array, (bx, by))
+        losses = loss_fn(model, bx, by, reduce=False)
+        loss += mx.sum(losses).item()
+
+    return loss / (len(targets) // batch_size)
 
 
 def main():
@@ -62,12 +74,11 @@ def main():
         loss, grads = loss_and_grad_fn(model, inputs, targets)
         optimizer.update(model, grads)
 
-    train_iterator = Data.iterate_batches(32, context_size, train_data)
-    x, y = map(mx.array, next(train_iterator))
-    loss = loss_fn(model, x, y)
-    print(f"{loss=}")
+    validation_loss = eval_fn(model, validation_data, context_size, 32)
+    print(f"{validation_loss=}")
 
     tic = time.perf_counter()
+    train_iterator = Data.iterate_batches(32, context_size, train_data)
     for train_iter, (inputs, targets) in zip(range(10000), train_iterator):
         xb, yb = map(mx.array, (inputs, targets))
         step(xb, yb)
@@ -75,8 +86,8 @@ def main():
     toc = time.perf_counter()
     print(f"Optimization took: {(toc-tic):.3f}s")
 
-    loss = loss_fn(model, x, y)
-    print(f"{loss=}")
+    validation_loss = eval_fn(model, validation_data, context_size, 32)
+    print(f"{validation_loss=}")
 
     print("\nFinal Sample:")
     idx = mx.zeros((1, 1), dtype=mx.uint32)
